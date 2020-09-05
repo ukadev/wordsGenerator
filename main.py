34ac5b87766f6e28efcf09478ce35525f6d01bcd
Ukadev <mail@ukadev.com>
2020-09-05 22:15:32 +0200

    Added copy results for each tab

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Application:
     def main(self):
         window = tk.Tk()
         window.title("Generador de Palabras")
-        window.geometry("810x570")
+        window.geometry("830x570")
         window.resizable(False, False)
 
         tabControl = ttk.Notebook(window)
@@ -24,34 +24,45 @@ class Application:
 
         # Words Textarea
         self.text_areaPairs = scrolledtext.ScrolledText(tab1, wrap=tk.WORD, width=40, height=30)
-        self.text_areaPairs.grid(column=1, padx=10, pady=10, row=2)
+        self.text_areaPairs.grid(column=1, padx=10, pady=10, row=2, rowspan=2)
         self.text_areaPairs.focus()
 
         # Pairs Textarea result
         self.text_area_resultPairs = scrolledtext.ScrolledText(tab1, wrap=tk.WORD, width=40, height=30, state='disabled')
-        self.text_area_resultPairs.grid(column=3, padx=25, row=2)
+        self.text_area_resultPairs.grid(column=3, padx=25, row=2, rowspan=2)
 
         # Submit Button
         self.submitPairs = Button(tab1, text='Crear Parejas', command=self.executePairs)
         self.submitPairs.grid(column=2, row=2, pady=10)
 
+        # Copy Button Pairs
+        self.copyPairs = Button(tab1, text='Copiar Resultado', command=self.copyPairs)
+        self.copyPairs.grid(column=2, row=3, pady=10)
+
         # Title Label
         ttk.Label(tab2,
                   text="Introduce las palabras en el bloque izquierdo, una por línea, sin separar por comas ni ningún símbolo y luego pulsa el botón 'Crear Gemelos'",
                   background='green', foreground="white").grid(column=1, row=1, columnspan=3, pady=10)
+
         # Words Textarea Twins
         self.text_areaTwins = scrolledtext.ScrolledText(tab2, wrap=tk.WORD, width=40, height=30)
-        self.text_areaTwins.grid(column=1, padx=10, pady=10, row=2)
+        self.text_areaTwins.grid(column=1, padx=10, pady=10, row=2, rowspan=2)
         self.text_areaTwins.focus()
 
         # Pairs Textarea result
         self.text_area_resultTwins = scrolledtext.ScrolledText(tab2, wrap=tk.WORD, width=40, height=30, state='disabled')
-        self.text_area_resultTwins.grid(column=3, padx=25, row=2)
+        self.text_area_resultTwins.grid(column=3, padx=25, row=2, rowspan=2)
 
         # Submit Button
         self.submitTwins = Button(tab2, text='Crear gemelos', command=self.executeTwins)
         self.submitTwins.grid(column=2, row=2, pady=10)
 
+        # Copy Button Twins
+        self.copyTwins = Button(tab2, text='Copiar Resultado', command=self.copyTwins)
+        self.copyTwins.grid(column=2, row=3, pady=10)
+
+        tabControl.bind("<1>", self.clearTextAreas())
+        # tab2.bind("<<NotebookTabChanged>>", self.clearTextAreas())
 
         window.mainloop()
 
@@ -90,13 +101,35 @@ class Application:
         return result
 
     def generateTwins(self, array):
-        alreadyProcessed = []
         result = ""
 
         for first in array:
             result += first+":"+first+"\r\n"
         return result
 
+    def clearTextAreas(self):
+        self.text_area_resultTwins.config(state="normal")
+        self.text_area_resultTwins.delete('1.0', "end")
+        self.text_area_resultTwins.config(state="disabled")
+        self.text_areaTwins.delete('1.0', "end")
+        self.text_area_resultPairs.config(state="normal")
+        self.text_area_resultPairs.delete('1.0', "end")
+        self.text_area_resultPairs.config(state="disabled")
+        self.text_areaPairs.delete('1.0', "end")
+
+    def copyPairs(self):
+        clip = tk.Tk()
+        clip.withdraw()
+        clip.clipboard_clear()
+        clip.clipboard_append(self.text_area_resultPairs.get("1.0", "end"))
+        clip.destroy()
+
+    def copyTwins(self):
+        clip = tk.Tk()
+        clip.withdraw()
+        clip.clipboard_clear()
+        clip.clipboard_append(self.text_area_resultTwins.get("1.0", "end"))
+        clip.destroy()
 
 if __name__ == "__main__":
     app = Application()
