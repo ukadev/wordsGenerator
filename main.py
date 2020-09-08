62e5f751ec7320346c791ad113243cfc4c7cb20c
Ukadev <mail@ukadev.com>
2020-09-08 17:33:31 +0200

    Re-organize code. Added clear textareares on tab change.

--- main.py
+++ main.py
@@ -23,79 +23,80 @@ class Application:
         self.twins(tab2)
         self.uniqueTwins(tab3)
 
-        tabControl.bind("<1>", self.clearTextAreas())
+        tabControl.bind("<1>", self.clearTextAreas)
+
         window.mainloop()
 
-    def pairs(self, tab1):
+    def pairs(self, tab):
         # Title Label
-        ttk.Label(tab1,
+        ttk.Label(tab,
                   text="Introduce las palabras en el bloque izquierdo, una por línea, sin separar por comas ni ningún símbolo y luego pulsa el botón 'Crear Parejas'",
                   background='green', foreground="white").grid(column=1, row=1, columnspan=3, pady=10)
 
         # Words Textarea
-        self.text_areaPairs = scrolledtext.ScrolledText(tab1, wrap=tk.WORD, width=40, height=30)
+        self.text_areaPairs = scrolledtext.ScrolledText(tab, wrap=tk.WORD, width=40, height=30)
         self.text_areaPairs.grid(column=1, padx=10, pady=10, row=2, rowspan=2)
         self.text_areaPairs.focus()
 
         # Pairs Textarea result
-        self.text_area_resultPairs = scrolledtext.ScrolledText(tab1, wrap=tk.WORD, width=40, height=30,
+        self.text_area_resultPairs = scrolledtext.ScrolledText(tab, wrap=tk.WORD, width=40, height=30,
                                                                state='disabled')
         self.text_area_resultPairs.grid(column=3, padx=25, row=2, rowspan=2)
 
         # Submit Button
-        self.submitPairs = Button(tab1, text='Crear Parejas', command=self.executePairs)
+        self.submitPairs = Button(tab, text='Crear Parejas', command=self.executePairs)
         self.submitPairs.grid(column=2, row=2, pady=10)
 
         # Copy Button Pairs
-        self.copyPairs = Button(tab1, text='Copiar Resultado', command=self.copyPairs)
+        self.copyPairs = Button(tab, text='Copiar Resultado', command=self.copyPairs)
         self.copyPairs.grid(column=2, row=3, pady=10)
 
-    def twins(self, tab2):
+    def twins(self, tab):
         # Title Label
-        ttk.Label(tab2,
+        ttk.Label(tab,
                   text="Introduce las palabras en el bloque izquierdo, una por línea, sin separar por comas ni ningún símbolo y luego pulsa el botón 'Crear Gemelos'",
                   background='green', foreground="white").grid(column=1, row=1, columnspan=3, pady=10)
 
         # Words Textarea Twins
-        self.text_areaTwins = scrolledtext.ScrolledText(tab2, wrap=tk.WORD, width=40, height=30)
+        self.text_areaTwins = scrolledtext.ScrolledText(tab, wrap=tk.WORD, width=40, height=30)
         self.text_areaTwins.grid(column=1, padx=10, pady=10, row=2, rowspan=2)
         self.text_areaTwins.focus()
 
         # Pairs Textarea result
-        self.text_area_resultTwins = scrolledtext.ScrolledText(tab2, wrap=tk.WORD, width=40, height=30,
+        self.text_area_resultTwins = scrolledtext.ScrolledText(tab, wrap=tk.WORD, width=40, height=30,
                                                                state='disabled')
         self.text_area_resultTwins.grid(column=3, padx=25, row=2, rowspan=2)
 
         # Submit Button
-        self.submitTwins = Button(tab2, text='Crear gemelos', command=self.executeTwins)
+        self.submitTwins = Button(tab, text='Crear gemelos', command=self.executeTwins)
         self.submitTwins.grid(column=2, row=2, pady=10)
 
         # Copy Button Twins
-        self.copyTwins = Button(tab2, text='Copiar Resultado', command=self.copyTwins)
+        self.copyTwins = Button(tab, text='Copiar Resultado', command=self.copyTwins)
         self.copyTwins.grid(column=2, row=3, pady=10)
 
-    def uniqueTwins(self, tab3):
+    def uniqueTwins(self, tab):
         # Title Label
-        ttk.Label(tab3,
+        ttk.Label(tab,
                   text="Introduce las palabras en el bloque izquierdo, una por línea, sin separar por comas ni ningún símbolo y luego pulsa el botón 'Crear Gemelos Únicos'",
                   background='green', foreground="white").grid(column=1, row=1, columnspan=3, pady=10)
 
         # Words Textarea Unique Twins
-        self.text_areaUniqueTwins = scrolledtext.ScrolledText(tab3, wrap=tk.WORD, width=40, height=30)
+        self.text_areaUniqueTwins = scrolledtext.ScrolledText(tab, wrap=tk.WORD, width=40, height=30)
         self.text_areaUniqueTwins.grid(column=1, padx=10, pady=10, row=2, rowspan=2)
         self.text_areaUniqueTwins.focus()
 
         # Unique twins Textarea result
-        self.text_area_resultUniqueTwins = scrolledtext.ScrolledText(tab3, wrap=tk.WORD, width=40, height=30,
+        self.text_area_resultUniqueTwins = scrolledtext.ScrolledText(tab, wrap=tk.WORD, width=40, height=30,
                                                                      state='disabled')
         self.text_area_resultUniqueTwins.grid(column=3, padx=25, row=2, rowspan=2)
 
         # Submit Unique Twins Button
-        self.submitUniqueTwins = Button(tab3, text='Crear gemelos únicos', command=self.executeUniqueTwins)
+        self.submitUniqueTwins = Button(tab, text='Crear gemelos únicos', command=self.executeUniqueTwins)
         self.submitUniqueTwins.grid(column=2, row=2, pady=10)
 
         # Copy Button Twins
-        self.copyUniqueTwins = Button(tab3, text='Copiar Resultado', command=self.copyUniqueTwins)
+        self.copyUniqueTwins = Button(tab, text='Copiar Resultado', command=self.copyUniqueTwins)
         self.copyUniqueTwins.grid(column=2, row=3, pady=10)
 
     def executePairs(self):
@@ -128,6 +129,7 @@ class Application:
         self.text_area_resultUniqueTwins.insert("insert", uniqueTwins)
         self.text_area_resultUniqueTwins.config(state="disabled")
 
+
     def generatePairs(self, array):
         alreadyProcessed = []
         result = ""
@@ -135,9 +137,9 @@ class Application:
         for first in array:
             for second in array:
                 combination = [first, second]
-                if (first == second):
+                if(first == second):
                     continue
-                result += first + ":" + second + "\r\n"
+                result += first+":"+second+"\r\n"
                 alreadyProcessed.append(combination)
         return result
 
@@ -145,8 +147,9 @@ class Application:
         result = ""
 
         for first in array:
-            result += first + ":" + first + "\r\n"
+            result += first+":"+first+"\r\n"
         return result
+
 
     def generateUniqueTwins(self, array):
         result = ""
@@ -154,11 +157,11 @@ class Application:
         for first in array:
             if first in loaded:
                 continue
-            result += first + ":" + first + "\r\n"
+            result += first+":"+first+"\r\n"
             loaded.append(first)
         return result
 
-    def clearTextAreas(self):
+    def clearTextAreas(self, event):
         self.text_area_resultTwins.config(state="normal")
         self.text_area_resultTwins.delete('1.0', "end")
         self.text_area_resultTwins.config(state="disabled")
@@ -192,7 +195,6 @@ class Application:
         clip.clipboard_clear()
         clip.clipboard_append(self.text_area_resultUniqueTwins.get("1.0", "end"))
         clip.destroy()
-
 
 if __name__ == "__main__":
     app = Application()
